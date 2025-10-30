9df08322afe5aa9164f0cf72156998637fefb51a
SanqDuonq <namsang0902s@gmail.com>
2025-10-30 22:38:36 +0700

    Leetcode-42: Solved

---
+++ 1. Array & String/42. Trapping Rain Water.ts
@@ -0,0 +1,27 @@
+function trap(height: number[]): number {
+  let left = 0,
+    right = height.length - 1;
+  let leftMax = 0,
+    rightMax = 0,
+    total = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] >= leftMax) {
+        leftMax = height[left];
+      } else {
+        total += leftMax - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= rightMax) {
+        rightMax = height[right];
+      } else {
+        total += rightMax - height[right];
+      }
+      right--;
+    }
+  }
+
+  return total;
+}
