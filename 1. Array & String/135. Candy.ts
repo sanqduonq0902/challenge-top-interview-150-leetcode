bccf95100932c85187154a557c5a1847b2aad381
SanqDuonq <namsang0902s@gmail.com>
2025-10-30 22:36:36 +0700

    Leetcode-135: Solved

---
+++ 1. Array & String/135. Candy.ts
@@ -0,0 +1,17 @@
+function candy(ratings: number[]): number {
+  let candy = new Array(ratings.length).fill(1);
+
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candy[i] = candy[i - 1] + 1;
+    }
+  }
+
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+    }
+  }
+
+  return candy.reduce((a, b) => a + b);
+}
