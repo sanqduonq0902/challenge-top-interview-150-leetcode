aeec2b021c19736657a28ee5bca1816f31acde21
SanqDuonq <namsang0902s@gmail.com>
2025-11-12 20:40:58 +0700

    Leetcode-61: Solved

---
+++ 8. Linked List/61. Rotate List.ts
@@ -0,0 +1,23 @@
+function rotateRight(head: ListNode | null, k: number): ListNode | null {
+  let length = 1;
+  let tail = head;
+
+  while (tail!.next) {
+    tail = tail!.next;
+    length++;
+  }
+
+  k = k % length;
+  if (k === 0) return head;
+
+  let newTail = head;
+  for (let i = 1; i < length - k; i++) {
+    newTail = newTail!.next;
+  }
+
+  const newHead = newTail!.next;
+  newTail!.next = null;
+  tail!.next = head;
+
+  return newHead;
+}
