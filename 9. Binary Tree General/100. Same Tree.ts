e4dea39cf5ff68a7b61f1562755c0b68a27a7f8f
SanqDuonq <namsang0902s@gmail.com>
2025-11-14 13:44:25 +0700

    Leetcode-100: Solved

---
+++ 9. Binary Tree General/100. Same Tree.ts
@@ -0,0 +1,9 @@
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true;
+
+  if (!p || !q) return false;
+
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
