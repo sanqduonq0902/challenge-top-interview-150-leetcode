759b03aed45279f8d19f8ceefcae68e3cc77dc2b
SanqDuonq <namsang0902s@gmail.com>
2025-11-24 22:53:35 +0700

    Leetcode-199: Solved

---
+++ 10. Binary Tree BFS/199. Binary Tree Right Side View.ts
@@ -0,0 +1,23 @@
+function rightSideView(root: TreeNode | null): number[] {
+  if (!root) return [];
+
+  const result: number[] = [];
+  const queue: TreeNode[] = [root];
+
+  while (queue.length) {
+    const levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift()!;
+
+      if (i === levelSize - 1) {
+        result.push(node.val);
+      }
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return result;
+}
