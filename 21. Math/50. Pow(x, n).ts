b8636326be222a0d0191361f5eda2cfa16acf832
SanqDuonq <namsang0902s@gmail.com>
2025-12-13 09:20:05 +0700

    Leetcode-50: Solved

---
+++ 21. Math/50. Pow(x, n).ts
@@ -0,0 +1,11 @@
+function myPow(x: number, n: number): number {
+    if (n === 0) return 1;
+
+    if (n < 0) return myPow(1 / x, -n);
+
+    if (n % 2 === 0) {
+        return myPow(x * x, Math.floor(n / 2));
+    }
+
+    return x * myPow(x * x, Math.floor((n - 1)  / 2));
+};
