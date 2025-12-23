1839d15abd1b60973d766bf0750a1c57b8862371
SanqDuonq <namsang0902s@gmail.com>
2025-12-23 17:19:12 +0700

    Leetcode-221: Solved

---
+++ 23. Multidimensional DP/97. Interleaving String.ts
@@ -0,0 +1,26 @@
+function isInterleave(s1: string, s2: string, s3: string): boolean {
+    const m = s1.length;
+    const n = s2.length;
+
+    if (m + n !== s3.length) return false;
+
+    const dp: boolean[][] = Array.from({ length: m + 1}, () => Array(n + 1).fill(false));
+    
+    dp[0][0] = true;
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            const k = i + j - 1;
+
+            if (i > 0 && s1[i - 1] === s3[k] && dp[i - 1][j]) {
+                dp[i][j] = true;
+            }
+
+            if (j > 0 && s2[j - 1] === s3[k] && dp[i][j - 1]) {
+                dp[i][j] = true;
+            }
+        }
+    }
+
+    return dp[m][n];
+};
