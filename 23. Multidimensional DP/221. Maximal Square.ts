41251af3956f2e30edfb27f50fe22e17eb7d2b9d
SanqDuonq <namsang0902s@gmail.com>
2025-12-24 21:18:52 +0700

    Leetcode-123: Solved

---
+++ 23. Multidimensional DP/221. Maximal Square.ts
@@ -0,0 +1,23 @@
+function maximalSquare(matrix: string[][]): number {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    const dp: number[][] = Array.from({ length: m }, () => Array(n).fill(0));
+    let max = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === '1') {
+                if (i === 0 || j === 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+                }
+            }
+            max = Math.max(max, dp[i][j]);
+        }
+    }
+
+    return max * max;
+};
